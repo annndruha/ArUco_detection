cc11a7586b4034cc6600aa91ae7e94119e1e51e6
Андрей Маракулин <a.p.marakulin@gmail.com>
2020-03-27 21:40:14 +0300

    Work

--- web2.0.py
+++ web2.0.py
@@ -1,30 +1,25 @@
-import cv2
-import numpy as np
 import itertools
-import datetime
 import time
-from imutils.video import VideoStream
+import datetime
 from flask import Flask, Response, redirect, request, url_for
 
-
-vs = VideoStream(src=0).start()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     if request.headers.get('accept') == 'text/event-stream':
+
         def events():
             while True:
                 s = str(datetime.datetime.strftime(
                             datetime.datetime.now(
                             datetime.timezone(
                             datetime.timedelta(hours = 3))),'%Y.%m.%d  %H:%M:%S'))
-                yield "data: %s" % (s)
+                yield "data: %s\n\n" % (s,)
                 time.sleep(.1)
 
-
         return Response(events(), content_type='text/event-stream')
-    return redirect(url_for('static', filename='index.html'))
+    return redirect(url_for('static', filename='index2.html'))
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=80)
+    app.run(host='localhost', port=80) 
